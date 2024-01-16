53c9e379edd3e9c67cc9ec804e5766bbe35f9f17
dahlia <wlstnrdahlia@gmail.com>
2024-01-16 23:11:42 +0900

    🎨 Style: tailwind.config.ts 세팅, 반복 컴포넌트 출력 테스트
    - tailwind.config.ts dark/light 모드 분리하여 재사용가능한 스타일링 설정
    - 버튼 컴포넌트, 리스트 컴포넌트 출력 테스트 완료

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,21 @@
 export default function Home() {
+  const listNames : Array<string> = ["모드 전환", "설정", "정보"];
   return (
-    <main>
+    <main className="text-[50px] bg-light-bg dark:bg-dark-bg h-full">
+      {/* 팔로우 버튼 스타일링 테스트 */}
+      <div className="flex p-[20px]">
+        <button className="w-[104px] h-[34px] leading-[80%] text-[15px] font-semibold text-light-txt dark:text-dark-txt rounded-[10px] border-[1px] border-light-btn-brd dark:border-dark-btn-brd bg-inherit">팔로우</button>
+      </div>
+      {/* 게시 버튼 스타일링 테스트 */}
+      <div className="flex p-[20px]">
+        <button className="min-w-[58px] h-[36px] leading-[80%] text-[15px] font-semibold text-light-post-txt dark:text-dark-post-txt rounded-[80px] dark:border-dark-btn-brd bg-light-btn-brd dark:bg-dark-btn-brd">게시</button>
+      </div>
+      {/* 리스트, input Box shadow 스타일링 테스트*/}
+      <div className="text-[15px] dark:text-dark-txt text-light-txt font-normal dark:bg-dark-list-bg w-[174px] rounded-[15px] m-[20px] shadow-box dark:border-dark-hr">
+        <ul className="">
+          { listNames.map((list, idx) => <li key={idx} className="border-b-light-hr dark:border-b-dark-hr py-[10px] border-b-[1px] last:border-b-0 first:pt-[15px] last:pb-[15px] indent-[20px]">{list}</li>)}
+        </ul>
+      </div>
     </main>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,5 +7,40 @@ module.exports = {
     "./src/**/*.{ts,tsx}",
   ],
   theme: {
+    fontFamily: {
+      pre: ['var(--pre)'],
+    },
+    boxShadow: {
+      'box' : '0px 10px 20px 5px rgba(0, 0, 0, 0.05)',
+    },
+    colors: {
+      /* dark mode colors */
+      'dark-bg': '#101010',
+      'dark-list-bg': '#181818',
+      'dark-navicon': '#4D4D4D', 
+      'dark-navicon_slc': '#F3F5F7',
+      'dark-hr': 'rgba(243, 245, 247, 0.15)',
+      'dark-txt': '#F3F5F7',
+      'dark-sub-txt': '#777777',
+      'dark-post-txt': '#101010',
+      'dark-input-bg': '#0A0A0A',
+      'dark-btn-brd': 'rgba(243, 245, 247, 0.15)',
+      'dark-input-brd': 'rgba(243, 245, 247, 0.15)',
+      'dark-icon-hover': '#1C1C1C',
+      /* light mode colors */
+      'light-bg': '#FFFFFF',
+      'light-navicon': '#B8B8B8',
+      'light-navicon_slc': '#000000',
+      'light-hr': '#D9D9D9',
+      'light-txt': '#000000',
+      'light-sub-txt': '#777777',
+      'light-post-txt': '#FFFFFF',
+      'light-input-bg': '#FAFAFA',
+      'light-input-brd': 'rgba(0, 0, 0, 0.15)',
+      'light-btn-brd': 'rgba(0, 0, 0, 0.15)',
+      'light-icon-hover': '#F5F5F5',
+      /* Shared Styles */
+      'link-txt': '0095F6',
+    },
   }
 };
